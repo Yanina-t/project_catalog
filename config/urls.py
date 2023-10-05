9d2e895a3fa5486329f970d688382b4f69a34161
Yanina-t <vyaninat@gmail.com>
2023-10-05 17:13:24 +0300

    bug 3 [05/Oct/2023 17:11:39] "GET /admin/ HTTP/1.1" 500 80783

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('catalog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
